5d241ce9b1f8f132ccd133fb958a0fa83ee4a9ea
Alexander Guryanov <caiiiycuk@gmail.com>
2021-05-26 16:50:55 +0700

    rebuild with latest emscripten

--- lib/xtool/port.h
+++ lib/xtool/port.h
@@ -14,7 +14,13 @@ char* port_ltoa( long value, char* result, int base );
 char* port_ultoa( unsigned long value, char* result, int base );
 
 #if defined(__unix__) || defined(__APPLE__)
+#ifdef __cplusplus
+extern "C" {
+#endif
 char *strupr(char *string);
+#ifdef __cplusplus
+}
+#endif
 #endif
 
 #endif
